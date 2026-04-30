3e415fa59dc77d4ba6a545fa218151c6b77de5ec
Tuhin <tuhin.thakur1233@gmail.com>
2026-04-30 20:46:38 +0530

    refactor: Remove deprecated agent and user route controllers, models, and services

--- dist/api/v1/routes/user.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controller_1 = require("../controller/user/user.controller");
-const userRouter = (0, express_1.Router)();
-userRouter.patch("/update-user", user_controller_1.updateUser);
-exports.default = userRouter;
--- dist/models/agentRun/agentRun.model.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const agentRun_schema_1 = require("./agentRun.schema");
-const AgentRunModel = (0, mongoose_1.model)("agentRuns", agentRun_schema_1.agentRunSchema);
-exports.default = AgentRunModel;
